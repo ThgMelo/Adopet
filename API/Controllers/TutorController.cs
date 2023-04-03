75d1884243a4f7fe12117c64e34dd0ef8d0e8bd8
Thiago Melo <es.thg.melo@gmail.com>
2023-04-03 11:10:22 -0300

    Add project files.

---
+++ API/Controllers/TutorController.cs
@@ -0,0 +1,18 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TutorController : ControllerBase
+{
+    private static List<Tutor> tutores = new List<Tutor>();
+
+    [HttpPost]
+    public void AdicionaTutor(Tutor tutor)
+    {
+        tutores.Add(tutor);
+        Console.WriteLine(tutor.Nome);
+        Console.WriteLine(tutor.Email); ;
+    }
+}
