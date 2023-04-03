7c765854f80195a9691b2956f54946fe4021cc65
Thiago Melo <es.thg.melo@gmail.com>
2023-04-03 11:47:44 -0300

    feat: Validação de parâmetros da classe Tutor

--- API/Models/Tutor.cs
+++ API/Models/Tutor.cs
@@ -1,8 +1,18 @@
-﻿namespace API.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models;
 
 public class Tutor
 {
+    [Required(ErrorMessage = "O nome do tutor é obrigatório")]
     public string Nome { get; set; }
+
+
+    [Required(ErrorMessage = "O email do tutor é obrigatório.")]
     public string Email { get; set; }
+
+
+    [Required(ErrorMessage = "A senha do tutor é obrigatório")]
+    [MinLength(8, ErrorMessage = "A senha do tutor deve conter, no mínino, 8 caracteres")]
     public string Senha { get; set; }
 }
